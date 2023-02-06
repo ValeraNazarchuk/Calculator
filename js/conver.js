a15d102cd42d6f6899fd484ee231070b9c98bff3
ValeraNazarchuk <valera55555nazarchuk@gmail.com>
2023-02-06 14:49:10 +0200

    the currency calculation logic is finished~

--- js/conver.js
+++ js/conver.js
@@ -1,20 +1,35 @@
 const values = document.querySelectorAll('.canver__box-value')
 
+const select = document.querySelector('#select')
+const input = document.querySelector('#input')
+const result = document.querySelector('#result')
+
 async function getCurrencies () {
+  const currency = {}
+
   const url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
 
   const response = await fetch(url)
   const data = await response.json();
-
-  console.log(data);
-
+  
   for (let item of values) {
     for (let el of data) {
       if (item.dataset.value === el.cc) [
+        currency[el.cc] = el.rate.toFixed(2),
         item.textContent = el.rate.toFixed(2)
       ]
     }
   }
+  return currency
 }
 
-getCurrencies()
+getCurrencies().then(elem => {
+  input.oninput = converValue
+  select.oninput = converValue
+
+  function converValue() {
+    result.value = ((input.value) / elem[select.value]).toFixed(2)
+  }
+
+  converValue()
+})
